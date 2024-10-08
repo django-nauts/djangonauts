77ec90b63957998643e4404eb87ee69929c0fbf5
djangonauts <djangonauts@gmail.com>
2024-10-09 00:19:22 +0330

    Add production config

---
+++ config/production.py
@@ -0,0 +1,13 @@
+from .settings import *  # Import everything from settings.py
+
+# Override the settings specific to production
+
+SECRET_KEY = 'django-insecure-75$7^wu+ilhv^68^vg*b88v7!&qcv1l$@p@j_#3grst=sro)td'
+
+DEBUG = False
+
+ALLOWED_HOSTS = ['127.0.0.1', 'djangonauts.me', 'www.djangonauts.me', 'https://djangonauts.me']
+
+# Change media address based on your website
+MEDIA_ROOT = "/home/djangona/djangonauts/media.djangonauts.me/"
+MEDIA_URL = "https://media.djangonauts.me/"
--- config/settings.py
+++ config/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-75$7^wu+ilhv^68^vg*b88v7!&qcv1l$@p@j_#3grst=sro)td'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'djangonauts.me', 'www.djangonauts.me', 'https://djangonauts.me']
+ALLOWED_HOSTS = []
 
 
 # Application definition
