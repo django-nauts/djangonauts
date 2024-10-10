785a7f14806418fd8c3ba8c7c6d43219c282a983
djangonauts <djangonauts@gmail.com>
2024-10-11 00:21:27 +0330

    Change default post published status to True

--- blog/models.py
+++ blog/models.py
@@ -17,7 +17,7 @@ class Post(models.Model):
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
-    published = models.BooleanField(default=False)
+    published = models.BooleanField(default=True)
 
     class Meta:
         ordering = ['-updated']
